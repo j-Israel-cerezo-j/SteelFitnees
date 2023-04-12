3b7ebfb92e4ac3b06f962047612040214072c828
jacob israel cerezo juarez <j.israel.c.j@hotmail.com>
2023-04-11 20:44:40 -0600

    remove SteelFitnees from gitignore

---
+++ SteelFitnees/gentelella-master/production/productsByBranche.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SteelFitnees.gentelella_master.production
+{
+    public partial class productsByBranche : System.Web.UI.Page
+    {
+        public int getIdPorduct { get; set; } = -1;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.QueryString["id"]!=""&&Request.QueryString!=null)
+            {
+                getIdPorduct = Convert.ToInt32(Request.QueryString["id"]);
+            }
+
+        }
+    }
+}
