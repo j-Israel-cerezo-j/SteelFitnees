0c7795f49a4b6cdcd72717358d8e962ed12e86a0
jacob israel cerezo juarez <j.israel.c.j@hotmail.com>
2023-05-23 00:12:32 -0600

    landa

--- SteelFitnees/gentelella-master/production/js/personalizados/allBranches/msjOpeningBrancheCards.js
+++ SteelFitnees/gentelella-master/production/js/personalizados/allBranches/msjOpeningBrancheCards.js
@@ -5,18 +5,16 @@ function buildOpeningBranches() {
      const dayOfWeek = today.getDay();
      const todayName = daysOfWeek[dayOfWeek];
 
-    request(buildMsjShedulesCards, 'Handlers/sucursalesController.aspx?meth=day&dayName=' + todayName,false);
-}
-
-function buildMsjShedulesCards(json) {
+    request((json) => {
         json.forEach(item => {
-        var divMsjOpening = document.getElementById("msjOpening" + item.idSucursal)
-        if (divMsjOpening != undefined) {
-            var scheduleOpen = formant12HoursTime(item.horaInicio);
-            var scheduleClose = formant12HoursTime(item.horaCierre);
-            document.getElementById("msjSheduleCards" + item.idSucursal).innerText = scheduleOpen + " - " + scheduleClose;
-            openingStatusR(item.horaInicio, item.horaCierre, "msjOpening" + item.idSucursal);
-        }
-    })
+            var divMsjOpening = document.getElementById("msjOpening" + item.idSucursal)
+            if (divMsjOpening != undefined) {
+                var scheduleOpen = formant12HoursTime(item.horaInicio);
+                var scheduleClose = formant12HoursTime(item.horaCierre);
+                document.getElementById("msjSheduleCards" + item.idSucursal).innerText = scheduleOpen + " - " + scheduleClose;
+                openingStatusR(item.horaInicio, item.horaCierre, "msjOpening" + item.idSucursal);
+            }
+        })
+    }, 'Handlers/sucursalesController.aspx?meth=day&dayName=' + todayName, false);
 }
 setInterval(buildOpeningBranches, 60000)
